c6b918a54e8f14c6274766263af11fb687cab4e9
Jameson Shirley <jameson.shirley24@gmail.com>
2022-02-21 13:35:38 -0500

    Added Project Boilerplate

---
+++ software2project/src/main/java/com/example/software2project/LoginController.java
@@ -0,0 +1,23 @@
+package com.example.software2project;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private TextField localeDateTime;
+
+    @FXML
+    private TextField password;
+
+    @FXML
+    private TextField userID;
+
+    @FXML
+    void loginBtnClicked(ActionEvent event) {
+
+    }
+
+}
