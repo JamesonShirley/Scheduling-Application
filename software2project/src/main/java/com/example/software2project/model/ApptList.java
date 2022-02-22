cc72e8449316b7a8f39ddc9844fb063ad723d9eb
Jameson Shirley <jameson.shirley24@gmail.com>
2022-02-22 13:45:06 -0500

    added appointments and apptlist code to classes

--- software2project/src/main/java/com/example/software2project/model/ApptList.java
+++ software2project/src/main/java/com/example/software2project/model/ApptList.java
@@ -1,4 +1,29 @@
 package com.example.software2project.model;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class ApptList {
+    private static ObservableList<Appointment> apptlist = FXCollections.observableArrayList();
+    public static ObservableList<Appointment> getAllAppt(){return apptlist;}
+    public static void addAppt(Appointment appt) {apptlist.add(appt);}
+    public static boolean deleteAppt(int id){
+        for (Appointment appt : ApptList.getAllAppt()){
+            if (appt.getId() == id){
+                return ApptList.getAllAppt().remove(appt);
+            }
+        }
+        return false;
+    }
+    public static Appointment lookupAppt(int id){
+        for (Appointment appt : ApptList.getAllAppt()){
+            if (appt.getId() == id){
+                return appt;
+            }
+        }
+        return null;
+    }
+    public static void deleteAllAppt(){
+        ApptList.getAllAppt().clear();
+    }
 }
