d313dd89256e54c1525e5afa617c0bdcce452b98
Jameson Shirley <jameson.shirley24@gmail.com>
2022-02-23 12:02:13 -0800

    Added functionality

--- software2project/src/main/java/com/example/software2project/controller/LoginController.java
+++ software2project/src/main/java/com/example/software2project/controller/LoginController.java
@@ -8,24 +8,48 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.time.ZoneId;
+import java.util.Locale;
 
 public class LoginController {
+    public void initialize(){
+        localeDateTime.setText(String.valueOf(ZoneId.of(ZoneId.systemDefault().getId())));
+        Locale locale = Locale.getDefault();
+        System.out.println(locale.getDisplayLanguage());
+    }
     Stage stage;
     Parent scene;
+
+    @FXML
+    private Label localTimeZone;
 
     @FXML
     private TextField localeDateTime;
 
     @FXML
+    private Button login;
+
+    @FXML
     private TextField password;
 
     @FXML
+    private Label passwordText;
+
+    @FXML
     private TextField userID;
+
+    @FXML
+    private Label usernameText;
+
+    @FXML
+    private Text welcome;
 
     @FXML
     void loginBtnClicked(ActionEvent event) throws IOException, SQLException {
